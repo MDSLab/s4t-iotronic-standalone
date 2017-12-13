f6259ec0c2ddfd3d2d02c195d7d2ef0bdaa677ed
Nicola Peditto <npeditto@unime.it>
2017-12-13 16:39:01 +0100

    New plugin examples.

---
+++ examples/plugins.examples/echo.js
@@ -0,0 +1,15 @@
+//pluginjsonschema = {"says": "BlaBlaBla..."}
+
+exports.main = function (arguments, callback){ 
+  
+  var says = arguments.says;
+  
+  var LIGHTNINGROD_HOME = process.env.LIGHTNINGROD_HOME;
+  api = require(LIGHTNINGROD_HOME + '/modules/plugins-manager/plugin-apis');
+  logger = api.getLogger();
+  
+  logger.info(says); 
+  
+  callback("OK", says);
+  
+}
---
+++ examples/plugins.examples/hello.js
@@ -0,0 +1,18 @@
+//pluginjsonschema = {"name": "IoTronic"}
+
+exports.main = function (arguments){ 
+  
+    var name = arguments.name;
+  
+    var LIGHTNINGROD_HOME = process.env.LIGHTNINGROD_HOME;
+    api = require(LIGHTNINGROD_HOME + '/modules/plugins-manager/plugin-apis');
+    logger = api.getLogger();
+
+    logger.info("Hello plugin starting...");
+  
+    setInterval(function(){ 
+	logger.info('Hello '+name+'!'); 
+      
+    }, 3000); 
+  
+}
